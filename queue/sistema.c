a93a66c3a4ecd69805f0629eff31b9f4ed0fe0b4
carlost <carlos.taffe@gmail.com>
2020-05-09 10:13:04 -0300

    intro ipc con manejo actuadores -> led

--- queue/sistema.c
+++ queue/sistema.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <pigpio.h>
 
 typedef enum {false,true} bool_t;
 bool_t actuador[10];
@@ -12,6 +13,7 @@ void *sensores() {
 	while (1){
 		scanf("%d", &nro_actuador);
 		if (actuador[nro_actuador] == false) actuador[nro_actuador] = true; else actuador[nro_actuador] = false;
+		if (nro_actuador == 5) if (actuador[5] == false) gpioWrite(14, 0); else gpioWrite(14, 1); //cambia el estado SOLO si se oprime 5
 	}
 	pthread_exit(NULL);
 }
@@ -35,6 +37,8 @@ int main (){
 	for (i=0;i<10;i++){	//inicializa las salidas en bajo
 		actuador[i] = false;
 	}
+  	gpioSetMode(14, PI_OUTPUT);  // Set GPIO14 as OUTput.
+	gpioWrite(14, 0); // Set GPIO14 low.
 	pthread_create (&tid1, NULL, sensores, NULL);
 	pthread_create (&tid2, NULL, actuadores, NULL);
 	pthread_join (tid1,NULL);
